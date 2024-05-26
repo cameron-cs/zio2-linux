ce14cf1d379911128ff94387f458dc7a146087b0
Cameron <camerongadzh@gmail.com>
2024-05-26 21:01:30 +1000

    fixes

--- src/test/scala/LinuxIOServiceSpec.scala
+++ src/test/scala/LinuxIOServiceSpec.scala
@@ -10,24 +10,24 @@ object LinuxIOServiceSpec extends ZIOSpecDefault {
     test("open and close should work correctly") {
       for {
         _      <- ZIO.attempt(new java.io.File("/tmp/testfile").createNewFile())
-        fd     <- LinuxIOService.open("/tmp/testfile", 0).provideLayer(LinuxIOService.live)
-        result <- LinuxIOService.close(fd).provideLayer(LinuxIOService.live)
+        fd     <- LinuxIOService.open("/tmp/testfile", 0)
+        result <- LinuxIOService.close(fd)
       } yield assert(result)(equalTo(0)) // close should return 0 on success
     },
     test("read and write should work correctly") {
       val value = 12345678L
       for {
         _          <- ZIO.attempt(new java.io.File("/tmp/testfile").createNewFile())
-        fd         <- LinuxIOService.open("/tmp/testfile", 2).provideLayer(LinuxIOService.live) // O_RDWR flag
+        fd         <- LinuxIOService.open("/tmp/testfile", 2) // O_RDWR flag
         source     <- ZIO.succeed(new Memory(8))
         _          = source.setLong(0, value)
-        _          <- LinuxIOService.write(fd, source, 8).provideLayer(LinuxIOService.live)
-        _          <- LinuxIOService.lseek(fd, 0, 0).provideLayer(LinuxIOService.live) // Seek to the start of the file
+        _          <- LinuxIOService.write(fd, source, 8)
+        _          <- LinuxIOService.lseek(fd, 0, 0) // Seek to the start of the file
         readBuffer <- ZIO.succeed(new Memory(8))
-        _          <- LinuxIOService.read(fd, readBuffer, 8).provideLayer(LinuxIOService.live)
+        _          <- LinuxIOService.read(fd, readBuffer, 8)
         readValue  = readBuffer.getLong(0)
-        _          <- LinuxIOService.close(fd).provideLayer(LinuxIOService.live)
+        _          <- LinuxIOService.close(fd)
       } yield assert(readValue)(equalTo(value)) // read value should match the written value
     }
-  )
+  ).provideLayer(LinuxIOService.live)
 }
--- src/test/scala/LinuxMemoryServiceSpec.scala
+++ src/test/scala/LinuxMemoryServiceSpec.scala
@@ -7,45 +7,41 @@ import zio.test.Assertion.*
 object LinuxMemoryServiceSpec extends ZIOSpecDefault {
 
   def spec = suite("LinuxMemoryServiceSpec")(
-
     test("memcpy should copy memory correctly") {
       val value = 12345678L
       for {
-        source      <- LinuxMemoryService.malloc(8).provideLayer(LinuxMemoryService.live)
-        dest        <- LinuxMemoryService.malloc(8).provideLayer(LinuxMemoryService.live)
+        source      <- LinuxMemoryService.malloc(8)
+        dest        <- LinuxMemoryService.malloc(8)
         _           = source.setLong(0, value)
-        _           <- LinuxMemoryService.memcpy(dest, source, 8).provideLayer(LinuxMemoryService.live)
+        _           <- LinuxMemoryService.memcpy(dest, source, 8)
         copiedValue = dest.getLong(0)
       } yield assert(copiedValue)(equalTo(value)) // copied value should match the source value
     },
-
     test("calloc should allocate memory") {
       for {
-        mem   <- LinuxMemoryService.calloc(1L, 8L).provideLayer(LinuxMemoryService.live)
+        mem   <- LinuxMemoryService.calloc(1L, 8L)
         _     = mem.setLong(0, 12345678L)
         value = mem.getLong(0)
-        _     <- LinuxMemoryService.free(mem).provideLayer(LinuxMemoryService.live)
+        _     <- LinuxMemoryService.free(mem)
       } yield assert(value)(equalTo(12345678L))
     },
-
     test("realloc should reallocate memory") {
       val value = 12345678L
       for {
-        mem          <- LinuxMemoryService.calloc(1L, 8L).provideLayer(LinuxMemoryService.live)
+        mem          <- LinuxMemoryService.calloc(1L, 8L)
         _            = mem.setLong(0, value)
-        reallocMem   <- LinuxMemoryService.realloc(mem, 16).provideLayer(LinuxMemoryService.live)
+        reallocMem   <- LinuxMemoryService.realloc(mem, 16)
         reallocValue = reallocMem.getLong(0)
-        _            <- LinuxMemoryService.free(reallocMem).provideLayer(LinuxMemoryService.live)
+        _            <- LinuxMemoryService.free(reallocMem)
       } yield assert(reallocValue)(equalTo(value))
     },
-
     test("memset should set memory correctly") {
       for {
-        ptr   <- LinuxMemoryService.calloc(1L, 8L).provideLayer(LinuxMemoryService.live)
-        _     <- LinuxMemoryService.memset(ptr, 0xFF, 8).provideLayer(LinuxMemoryService.live)
+        ptr   <- LinuxMemoryService.calloc(1L, 8L)
+        _     <- LinuxMemoryService.memset(ptr, 0xFF, 8)
         value = (0 until 8).map(ptr.getByte(_).toInt & 0xFF).toArray
-        _     <- LinuxMemoryService.free(ptr).provideLayer(LinuxMemoryService.live)
+        _     <- LinuxMemoryService.free(ptr)
       } yield assert(value)(equalTo(Array.fill(8)(0xFF)))
     }
-  )
+  ).provideLayer(LinuxMemoryService.live)
 }
